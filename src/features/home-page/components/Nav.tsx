01c9f64519947e5689e8e2afd9c566006882a5aa
Daniel Apodaca <daniela@zoomcatalog.com>
2023-05-12 15:04:53 -0500

    navigate to sign in page from nav

--- src/features/home-page/components/Nav.tsx
+++ src/features/home-page/components/Nav.tsx
@@ -1,6 +1,8 @@
 import { Button, Container, Group, createStyles } from '@mantine/core'
 import Image from 'next/image'
+import Link from 'next/link'
 import { DAILA_BLACK_LOGO } from '~/data/assets'
+import { SIGN_IN_PAGE } from '~/data/routes'
 
 const useStyles = createStyles(() => ({
   nav: {
@@ -23,7 +25,9 @@ export default function Nav() {
         <Group position="apart" w="100%">
           <Image {...DAILA_BLACK_LOGO} alt={DAILA_BLACK_LOGO.alt} />
           <Group>
-            <Button variant="default">Iniciar sesión</Button>
+            <Link href={SIGN_IN_PAGE.path}>
+              <Button variant="default">Iniciar sesión</Button>
+            </Link>
             <Button>Regístrate</Button>
           </Group>
         </Group>
